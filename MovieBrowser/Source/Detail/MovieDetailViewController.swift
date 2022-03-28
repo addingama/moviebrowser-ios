d20f71d65a56c6fa11d9896b773d2f0016f902b4
Addin Gama Bertaqwa <addingama@gmail.com>
2022-03-28 21:58:26 +0800

    movie detail with image

--- MovieBrowser/Source/Detail/MovieDetailViewController.swift
+++ MovieBrowser/Source/Detail/MovieDetailViewController.swift
@@ -18,11 +18,26 @@ class MovieDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         titleLabel.text = movieManager.selectedMovie?.title
         releaseLabel.text = movieManager.selectedMovie?.getReleaseDate()
         descriptionLabel.text = movieManager.selectedMovie?.overview
-        
+        posterImage.loadFrom(URLAddress: "https://image.tmdb.org/t/p/w500\(movieManager.selectedMovie?.poster_path ?? "")")
         
     }
+}
+
+extension UIImageView {
+    func loadFrom(URLAddress: String) {
+            guard let url = URL(string: URLAddress) else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                if let imageData = try? Data(contentsOf: url) {
+                    if let loadedImage = UIImage(data: imageData) {
+                            self?.image = loadedImage
+                    }
+                }
+            }
+        }
 }
